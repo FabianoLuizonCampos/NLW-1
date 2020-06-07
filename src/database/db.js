482171b26bc6639da83a7289f4125e2375bd94bf
Fabiano Luizon Campos <fabianoluizoncampos@gmail.com>
2020-06-07 12:32:14 -0300

    Versão Final

--- src/database/db.js
+++ src/database/db.js
@@ -4,8 +4,11 @@ const sqlite3 = require("sqlite3").verbose();
 // criar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db");
 
+module.exports = db;
+
 
 // utilizar o objeto de banco de dados, para nossas operações
+/*
 db.serialize( () => {
 
     //Com comandos SQL eu vou:
@@ -70,7 +73,7 @@ db.serialize( () => {
     });
 
     // 4. Deletar um dado da tabela
-    //db.run(`DELETE FROM places WHERE id = ?`, [1], function //() {
+    //db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
     //    if(err) {
     //        return console.log(err);
     //    }
@@ -78,3 +81,20 @@ db.serialize( () => {
     // } );
 });
 
+*/
+
+
+
+//db.run(`DELETE FROM places`);
+
+/*     
+db.all(`SELECT * FROM places`, function (err, rows) {
+        
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Aqui estão seus registros: ");
+        console.log(rows);
+    });
+
+*/
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const server = express();
 
+// pegar o banco de dados /  não necessita o .js
+const db = require("./database/db.js");
+
 // utilizando o template engine
 const nunjucks = require("nunjucks");
 
@@ -14,6 +17,9 @@ nunjucks.configure("src/views", {
 
 // configurar pasta publica
 server.use(express.static("public"));
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({extended:true}));
 
 // configura caminhos da minha aplicação
 // página inicial
@@ -33,11 +39,85 @@ server.get("/", (req,res) => {
 })
 
 server.get("/create-point", (req,res) => {
+
+    //Interação com o BD
+    //req.query: Query Strings da nossa url
+    // Teste: console.log(req.query) para os dados do formulario
+
+    // Teste da pagina modal: return res.render("create-point.html", { saved: true});
     return res.render("create-point.html");
 })
 
+// Função acrescentada para realizar post para não apresentar dados no frontend, alterando a rota para o backend
+server.post("/save-point", (req,res) => {
+    
+    //inserir dados no BD
+    const query = `
+        INSERT INTO places (
+            image,
+            name,            
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+        `;
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ];
+
+    
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err);
+
+            return res.send("Erro no cadastro!!!");
+        }
+
+        // Retirar linhas para não apresentar dados no frontend somente para teste
+        //console.log("Cadastro com sucesso");
+        //console.log(this);
+
+        //return res.send("ok");
+        return res.render("create-point.html", { saved: true}  );
+    }
+
+    db.run(query, values, afterInsertData);    
+})
+
 server.get("/search", (req,res) => {
-    return res.render("search-results.html");
+    // Banco de dados - Funcionalidades
+    //Pegar os dados do banco de dados
+
+    const search = req.query.search;
+
+    if(search == "")
+    {
+        // pesquisa vazia
+        return res.render( "search-results.html", { total: 0 });
+
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        
+        if(err) {
+            return console.log(err);
+        }
+        
+        const total = rows.length;
+
+        //mostrar a página html com os dados do BD
+        return res.render( "search-results.html", { places: rows, total: total });
+    });
+    
 })
 
 //server.get("/search-results", (req,res) => {
